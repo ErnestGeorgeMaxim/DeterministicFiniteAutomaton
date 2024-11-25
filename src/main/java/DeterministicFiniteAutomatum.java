b6a93e58687b98dcf66b7069366aa5894a54797e
LupuDaniela <daniela.lupu39@yahoo.com>
2024-11-25 17:19:55 +0200

    class LambdaNFA

--- src/main/java/DeterministicFiniteAutomatum.java
+++ src/main/java/DeterministicFiniteAutomatum.java
@@ -4,5 +4,6 @@ import java.util.HashSet;
 public class DeterministicFiniteAutomatum {
     public static void main(String[] args) {
         State states = new State();
+        System.out.println(states.isEndNode());
     }
 }
