172306edab4ce1805c1021260662d2447854b33d
ErnestGeorgeMaxim <ernest.maxim@student.unitbv.ro>
2024-11-25 17:31:27 +0200

    Added DeterministicFiniteAutomatum.java

--- src/main/java/DeterministicFiniteAutomatum.java
+++ src/main/java/DeterministicFiniteAutomatum.java
@@ -1,5 +1,45 @@
-import java.util.Set;
-import java.util.HashSet;
+import java.util.*;
 
 public class DeterministicFiniteAutomatum {
+    private Set<State> states;
+    private Set<Character> alphabet;
+    private Map<State,Map<Character,State>> transitions;
+    private State initialState;
+    private Set<State> finalState;
+
+    public DeterministicFiniteAutomatum() {
+        states = new HashSet<>();
+        alphabet = new HashSet<>();
+        transitions = new HashMap<>();
+        finalState = new HashSet<>();
+    }
+
+    public void setInitialState(State state) {
+        state.setStartNode(true);
+        this.initialState = state;
+        states.add(state);
+    }
+
+    public void addFinalState(State state) {
+        state.setEndNode(true);
+        finalState.add(state);
+        states.add(state);
+    }
+
+    public void addTransition(State from, Character symbol,State to) {
+        alphabet.add(symbol);
+        states.add(from);
+        states.add(to);
+
+        transitions.computeIfAbsent(from, k -> new HashMap<>()).put(symbol, to);
+    }
+
+    public void printTransitions() {
+        for(State from : transitions.keySet()) {
+            for(Character symbol : transitions.get(from).keySet()) {
+                State to = transitions.get(from).get(symbol);
+                System.out.println(from + " --(" + symbol + ")-->" + to);
+            }
+        }
+    }
 }
