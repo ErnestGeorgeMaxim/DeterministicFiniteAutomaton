da9c55dc3f710cd5b11d3fd6ec41c364959ee26b
ErnestGeorgeMaxim <ernest.maxim@student.unitbv.ro>
2024-11-25 16:41:08 +0200

    Added State class and lombok for addnotations

--- src/main/java/DeterministicFiniteAutomatum.java
+++ src/main/java/DeterministicFiniteAutomatum.java
@@ -1,5 +1,8 @@
+import java.util.Set;
+import java.util.HashSet;
+
 public class DeterministicFiniteAutomatum {
     public static void main(String[] args) {
-
+        State states = new State();
     }
 }
---
+++ src/main/java/State.java
@@ -0,0 +1,22 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class State {
+    private static int nextId = 0;
+    private int id;
+    private boolean isStartNode;
+    private boolean isEndNode;
+
+    public State() {
+        this.id = nextId++;
+        this.isStartNode = false;
+        this.isEndNode = false;
+    }
+
+    @Override
+    public String toString() {
+        return "q" + this.id;
+    }
+}
